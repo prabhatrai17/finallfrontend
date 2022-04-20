de15f7a02be7af7a49fcb99b843e5c8d43459a15
prabhatrai17 <67636502+prabhatrai17@users.noreply.github.com>
2022-04-20 13:41:21 +0530

    Add files via upload

--- adminanduser2-master/src/Components/CourseSearchFilter.jsx
+++ adminanduser2-master/src/Components/CourseSearchFilter.jsx
@@ -1,37 +1,36 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import "./CourseSearchFilter.css";
-import Button from '@mui/material/Button';
-
-
+import Button from "@mui/material/Button";
 
 function CourseSearchFilter(props) {
   return (
     <div className="course-search-and-filter">
-    <div className="course-search">
-      <input
-        className="course-search-input"
-        type="text"
-        name="course"
-        placeholder="Search Course"
-      ></input>
-      <Button variant='outlined' id="searchCourse" className="btn">
-        Search
-      </Button>
-    </div>
-    <div className="course-filter">
-    <div className='filter1'>Filter By</div>
-      {/*
+      <div className="course-search">
+        <input
+          className="course-search-input"
+          type="text"
+          name="course"
+          placeholder="Search Course"
+        ></input>
+        <Button variant="outlined" id="searchCourse" className="btn">
+          Search
+        </Button>
+      </div>
+      <div className="course-filter">
+        <div className="filter1">Filter By</div>
+        {/*
       <Button variant="outlined" className="course-filter-elem"> StudentId</Button>
   <Button variant="outlined" className="course-filter-elem"> Student Name</Button> */}
-      <Button variant="outlined" className="course-filter-elem"> Student Course</Button>
-   
+        <Button variant="outlined" className="course-filter-elem">
+          {" "}
+          Institute
+        </Button>
+      </div>
     </div>
-  </div>
   );
 }
 
 export default CourseSearchFilter;
-
 
 // class CourseSearchFilter extends Component {
 //     render() {
@@ -50,11 +49,11 @@ export default CourseSearchFilter;
 //       </div>
 //       <div className="course-filter">
 //       <div className='filter1'>Filter By</div>
-        
+
 //         <Button variant="outlined" className="course-filter-elem"> StudentId</Button>
 //         <Button variant="outlined" className="course-filter-elem"> Student Name</Button>
 //         <Button variant="outlined" className="course-filter-elem"> Course</Button>
-     
+
 //       </div>
 //     </div>
 //         );
